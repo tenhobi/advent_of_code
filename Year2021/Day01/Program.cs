eff8d3b44404343fc55d0a2b0f3b38b3ff7deb3b
Honza Bittner <git@tenhobi.dev>
2021-12-01 09:43:52 +0100

    ♻️ Refactor day 01 to use LINQ

--- Year2021/Day01/Program.cs
+++ Year2021/Day01/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Shared;
@@ -11,42 +10,17 @@ public class Day01 : ISolver
 {
     public string SolveFirst(string inputFile)
     {
-        IEnumerable<string> lines = File.ReadLines(inputFile);
-        int? lastValue = null;
-        var increases = 0;
-        foreach (var line in lines)
-        {
-            var currentValue = int.Parse(line);
-
-            if (lastValue < currentValue)
-            {
-                increases++;
-            }
-
-            lastValue = currentValue;
-        }
-
+        List<int> numbers = File.ReadLines(inputFile).Select(int.Parse).ToList();
+        var increases = numbers.Zip(numbers.Skip(1)).Count(tuple => tuple.First < tuple.Second);
         return $"{increases}";
     }
 
     public string SolveSecond(string inputFile)
     {
-        IEnumerable<string> lines = File.ReadLines(inputFile);
-        int? lastValue = null;
-        var increases = 0;
-
-        List<int> numbers = lines.Select(int.Parse).ToList();
-        for (var i = 0; i < numbers.Count - 2; i++)
-        {
-            var currentValue = numbers[i] + numbers[i + 1] + numbers[i + 2];
-            if (lastValue < currentValue)
-            {
-                increases++;
-            }
-
-            lastValue = currentValue;
-        }
-
+        List<int> numbers = File.ReadLines(inputFile).Select(int.Parse).ToList();
+        var numbersInGroup = numbers.Zip(numbers.Skip(1), numbers.Skip(2))
+            .Select(tuple => tuple.First + tuple.Second + tuple.Third).ToList();
+        var increases = numbersInGroup.Zip(numbersInGroup.Skip(1)).Count(tuple => tuple.First < tuple.Second);
         return $"{increases}";
     }
 }
