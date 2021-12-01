a83edf14bcd0b4f06f6d60267c1f3f6b9481fa99
Honza Bittner <git@tenhobi.dev>
2021-12-01 08:53:48 +0100

    ✨ Implemented day 01

--- Year2021/Day01/Program.cs
+++ Year2021/Day01/Program.cs
@@ -1,3 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Shared;
 
-Console.WriteLine("Hello, World!");
+ISolver solver = new Day01();
+solver.PrintSolutions("input.txt");
+
+public class Day01 : ISolver
+{
+    public string SolveFirst(string inputFile)
+    {
+        IEnumerable<string> lines = File.ReadLines(inputFile);
+        int? lastValue = null;
+        var increases = 0;
+        foreach (var line in lines)
+        {
+            var currentValue = int.Parse(line);
+
+            if (lastValue < currentValue)
+            {
+                increases++;
+            }
+
+            lastValue = currentValue;
+        }
+
+        return $"{increases}";
+    }
+
+    public string SolveSecond(string inputFile)
+    {
+        IEnumerable<string> lines = File.ReadLines(inputFile);
+        int? lastValue = null;
+        var increases = 0;
+
+        List<int> numbers = lines.Select(int.Parse).ToList();
+        for (var i = 0; i < numbers.Count - 2; i++)
+        {
+            var currentValue = numbers[i] + numbers[i + 1] + numbers[i + 2];
+            if (lastValue < currentValue)
+            {
+                increases++;
+            }
+
+            lastValue = currentValue;
+        }
+
+        return $"{increases}";
+    }
+}
---
+++ Year2021/Shared/Solver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shared
+{
+    public interface ISolver
+    {
+        string SolveFirst(string inputFile);
+
+        string SolveSecond(string inputFile);
+
+        void PrintSolutions(string inputFile)
+        {
+            Console.WriteLine($"First task: {SolveFirst(inputFile)}");
+            Console.WriteLine($"Second task: {SolveSecond(inputFile)}");
+        }
+    }
+}
