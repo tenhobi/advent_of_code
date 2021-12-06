b87f9ec61bf3a766676d0dbe00a72f598f7b6054
Honza Bittner <git@tenhobi.dev>
2021-12-06 07:10:42 +0100

    ✨ Implement day 06

--- Year2021/Day06/Program.cs
+++ Year2021/Day06/Program.cs
@@ -10,13 +10,65 @@ public class Day06 : Solver
 {
     public override string SolveFirst(string inputFile)
     {
-        List<string> data = File.ReadLines(inputFile).ToList();
-        return "?";
+        List<byte> data = File.ReadLines(inputFile).ToList()[0].Split(",").Select(byte.Parse).ToList();
+        const byte daysToCompute = 80;
+        const byte initialNewValue = 8;
+        const byte oldNewValue = 6;
+
+        for (var i = 0; i < daysToCompute; i++)
+        {
+            var count = data.Count;
+            for (var j = 0; j < count; j++)
+            {
+                if (data[j] == 0)
+                {
+                    data.Add(initialNewValue);
+                    data[j] = oldNewValue;
+                    continue;
+                }
+
+                data[j]--;
+            }
+        }
+
+        return data.Count.ToString();
     }
 
     public override string SolveSecond(string inputFile)
     {
-        List<string> data = File.ReadLines(inputFile).ToList();
-        return "?";
+        List<int> inData = File.ReadLines(inputFile).ToList()[0].Split(",").Select(int.Parse).ToList();
+        const int daysToCompute = 256;
+        const byte initialNewValue = 8;
+        const byte oldNewValue = 6;
+
+        var data = new long [9];
+        inData.ForEach(i => data[i]++);
+
+        for (var i = 0; i < daysToCompute; i++)
+        {
+            var additions = new long [9];
+            for (var daysLeft = 0; daysLeft < data.Length; daysLeft++)
+            {
+                if (data[daysLeft] == 0) continue;
+
+                if (daysLeft == 0)
+                {
+                    additions[oldNewValue] += data[daysLeft];
+                    additions[initialNewValue] += data[daysLeft];
+                    data[daysLeft] = 0;
+                    continue;
+                }
+
+                additions[daysLeft - 1] += data[daysLeft];
+                data[daysLeft] = 0;
+            }
+
+            for (int j = 0; j < additions.Length; j++)
+            {
+                data[j] += additions[j];
+            }
+        }
+
+        return data.Sum().ToString();
     }
 }
