c48259f7c1ca43b083ecea59da3c2371f4650ea4
Honza Bittner <git@tenhobi.dev>
2021-12-07 10:32:26 +0100

    ✨ Implement day 07

--- Year2021/Day07/Program.cs
+++ Year2021/Day07/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Shared;
@@ -10,13 +11,35 @@ public class Day07 : Solver
 {
     public override string SolveFirst(string inputFile)
     {
-        List<string> data = File.ReadLines(inputFile).ToList();
-        return "?";
+        List<int> data = File.ReadLines(inputFile).ToList()[0].Split(",").Select(int.Parse).ToList();
+        data.Sort();
+        var median = data[data.Count / 2];
+        var sum = data.Sum(i => Math.Abs(i - median));
+        return sum.ToString();
     }
 
     public override string SolveSecond(string inputFile)
     {
-        List<string> data = File.ReadLines(inputFile).ToList();
-        return "?";
+        List<int> data = File.ReadLines(inputFile).ToList()[0].Split(",").Select(int.Parse).ToList();
+        var min = data.Min();
+        var max = data.Max();
+        var bestSum = int.MaxValue;
+
+        for (var center = min; center < max; center++)
+        {
+            var sum = 0;
+            foreach (var difference in data.Select(value => Math.Abs(center - value)))
+            {
+                for (var j = 1; j <= difference; j++)
+                {
+                    sum += j;
+                }
+            }
+
+            if (sum < bestSum) bestSum = sum;
+            ;
+        }
+
+        return bestSum.ToString();
     }
 }
